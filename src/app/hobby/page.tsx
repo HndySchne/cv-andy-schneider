6ff847842ca25272607736e790a186ed4bc56706
Andy <schne.andy@gmail.com>
2023-09-01 10:10:14 +0200

    hobby-correction-affichage

--- src/app/hobby/page.tsx
+++ src/app/hobby/page.tsx
@@ -7,31 +7,43 @@ function Hobby() {
   return (
     <div>
       <h2>
-       <FontAwesomeIcon icon={faTableTennisPaddleBall} /> LOISIRS
+        <FontAwesomeIcon icon={faTableTennisPaddleBall} /> LOISIRS
       </h2>
       <section className="Hobby-body">
         <h3>Groupes de musique</h3>
         <div className="music-pictures">
-          <a href="https://www.youtube.com/watch?v=WAyWwqZtx9A">
-            <img src='/pictures/Pole-Nord.webp' alt="pole nord" />
-          </a>
+          <iframe
+            src="https://www.youtube.com/embed/WAyWwqZtx9A?si=RDF9WgcMh8_M3k-i"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+          <iframe
+            src="https://www.youtube.com/embed/XvWl5rtVHPM?si=BvJCqXKZheA30I2d"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
           <a href="https://open.spotify.com/artist/1LCmfsSmkquRTUWO9rqBAJ?si=Gfbdjy7hQ_OEWIjKvplhfw">
-            <img src='/pictures/matched-by-mistake.webp' alt="matched by mistake" />
-          </a>
-          <a href="https://www.youtube.com/@andyschneider759">
-            <img src='/pictures/youtube.webp' alt="Andy covers" />
+            <img
+              src="/pictures/matched-by-mistake.webp"
+              alt="matched by mistake"
+            />
           </a>
         </div>
         <h3>Sport</h3>
         <div>
           <ul>
-            <li>Semi-marathon de Reims - 2022 : 2h04</li>
-            <li>Semi-marathon de Strasbourg - 2023 : 2h10</li>
-            <li>Semi-marathon de Reims - 2023 : à venir</li>
+            <li>Semi-marathon Reims - 2022</li>
+            <li>Semi-marathon Strasbourg - 2023</li>
+            <li>Semi-marathon Reims - 2023</li>
           </ul>
         </div>
         <h3>Lutherie amateur</h3>
-        <p>Réparations de plusieurs guitares et basses électriques (formation avec un luthier professionnel)</p>
+        <p>
+          Réparations de plusieurs guitares et basses électriques (formation
+          avec un luthier professionnel)
+        </p>
       </section>
     </div>
   );
